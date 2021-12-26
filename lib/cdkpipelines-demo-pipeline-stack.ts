522017757d9dc3bab16e7659475fc0f24f0fe6cf
Punya Deependra Ariyadewa <deep@Punyas-MacBook-Air.local>
2021-12-26 20:16:28 +0530

    Initial commit.

---
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -0,0 +1,39 @@
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
+
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkpipelinesDemoPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
+
+       // How it will be built and synthesized
+       synth: new ShellStep('Synth', {
+         // Where the source can be found
+         input: CodePipelineSource.gitHub('gnudeep/cdk-pipeline-demo', 'main'),
+         
+         // Install dependencies, build and run cdk synth
+         commands: [
+           'npm run clean',
+           'npm ci',
+           'npm update',
+           'npm run build',
+           'npx cdk synth'
+         ],
+       }),
+    });
+
+    // This is where we add the application stages
+    // ...
+    pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+        env: { account: '083793351300', region: 'us-east-2' }
+      }));
+      
+  }
+}
