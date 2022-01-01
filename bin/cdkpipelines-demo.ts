47cb6c5003eef3f79be14a3fc7dc8f1ac7abf200
Punya Deependra Ariyadewa <deep@Punyas-MacBook-Air.local>
2022-01-01 12:59:46 +0530

    Add AWS account ID and the default region.

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -2,10 +2,13 @@
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
+var defaultAccount = process.env.CDK_DEFAULT_ACCOUNT;
+var defaultRegion = process.env.CDK_DEFAULT_REGION;
+
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-  env: { account: '083793351300', region: 'us-east-2' },
+  env: { account: defaultAccount, region: defaultRegion },
 });
 
 app.synth();
